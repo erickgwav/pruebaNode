362501e58fe9e99a018c246b090536cd53990ab4
datcougar <werick290@gmail.com>
2023-06-16 21:10:25 -0600

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const appRoute = require('./routes/rutas.js');
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
